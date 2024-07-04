6e4e3fa00081f69ce052cab6e8edcba8e13d46e1
JNRBK <rafeekmorgan@yahoo.com>
2024-07-04 15:14:41 +0300

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 3- to_Str"""
+
+
+def to_str(n: float) -> str:
+    """
+    function to_str takes a float n and returns
+    the string representation of the float
+    """
+    return str(n)
