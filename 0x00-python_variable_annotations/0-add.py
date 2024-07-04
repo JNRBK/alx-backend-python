18c2cd1147f66b8786406add24ef3dd17cad3efd
JNRBK <rafeekmorgan@yahoo.com>
2024-07-04 14:45:50 +0300

    documented

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
+""" 0-add """
 def add(a: float, b: float) -> float:
+
     """
     a type-annotated function add that
     that takes a float a and float b as arguments
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 1- concat """
+def concat(str1: str, str2: str) -> str:
+
+    """
+    a type-annotated function concat that
+    that takes a string str1 and string str2 as arguments
+    and returns their concatenation
+    """
+    return str1 + str2
