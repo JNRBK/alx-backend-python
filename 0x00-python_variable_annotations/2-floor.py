71d5868801c49086ecc1296bed74665f75104127
JNRBK <rafeekmorgan@yahoo.com>
2024-07-04 15:10:39 +0300

    float returned int

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 2-floor """
+
+def floor(n: float) -> int:
+    """
+    function floor that takes a float n as
+    argument and returns the floor fo the float"""
+    return int(n)
